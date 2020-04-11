f334160aeead18a53001fafc08aceb49cbe68bac
Carlos Resendiz <carlos.resendiz@definityfirst.com>
2020-04-10 23:02:29 -0500

    Adding ExportAzureToBacpac

---
+++ MassUpdate/ExportAzureToBacpac/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ExportAzureToBacpac
+{
+    class Program
+    {
+        public static IConfigurationRoot configuration;
+        private static ConnectionConfiguration currentConfiguration;
+
+        public static object Log { get; private set; }
+
+        static void Main(string[] args)
+        {
+
+            // Create service collection            
+            ServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            // Create service provider            
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            // Print connection string to demonstrate configuration object is populated
+            //Console.WriteLine(configuration.GetConnectionString("DataConnection"));
+
+
+            RunExportBat runBat = new RunExportBat(configuration);
+            runBat.Run();
+
+        }
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            
+            // Build configuration
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            // Add access to generic IConfigurationRoot
+            serviceCollection.AddSingleton(configuration);
+
+            // Add app
+            serviceCollection.AddTransient<RunExportBat>();
+        }
+    }
+}
---
+++ MassUpdate/ExportAzureToBacpac/RunExportBat.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace ExportAzureToBacpac
+{
+    public class RunExportBat
+    {
+        // requires using Microsoft.Extensions.Configuration;
+        private readonly IConfiguration Configuration;
+        private ConnectionConfiguration currentConfiguration;
+
+        public RunExportBat(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            currentConfiguration = new ConnectionConfiguration();
+            currentConfiguration.SourceServerName = configuration.GetSection("SourceServerName").Value;
+            currentConfiguration.SourceEncryptConnection = configuration.GetSection("SourceEncryptConnection").Value;
+            currentConfiguration.TargetDirectory = configuration.GetSection("TargetDirectory").Value;
+            currentConfiguration.SourceUser = configuration.GetSection("SourceUser").Value;
+            currentConfiguration.SourcePassword = configuration.GetSection("SourcePassword").Value;
+            currentConfiguration.DatabasesTextFileName = configuration.GetSection("DatabasesTextFileName").Value;
+        }
+
+        public void Run()
+        {
+
+            Console.WriteLine("Exporting process started");
+
+            var currentBasePath = Directory.GetParent(AppContext.BaseDirectory).FullName;
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file = new System.IO.StreamReader($"{currentBasePath}\\{currentConfiguration.DatabasesTextFileName}");
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+                consumeExportBatFile(line);
+            }
+
+            file.Close();
+
+            Console.WriteLine("Exporting process finished");
+            Console.ReadKey();
+
+        }
+
+        private void consumeExportBatFile(string currentDatabase)
+        {
+            Console.WriteLine($"Executing Azure export to bacpac for {currentDatabase} database");
+            // var commandToRun = @"ExportFromAzureSQLDb.bat myserver.database.windows.net MyDatabase D:\ImportSQLFromAzure\MyDatabase.bacpac DatabaseUser DatabasePassword";
+            var targetFile = $"{currentConfiguration.TargetDirectory}\\{currentDatabase}.bacpac";
+            var args = $"{currentConfiguration.SourceServerName} {currentDatabase} {targetFile} {currentConfiguration.SourceUser} {currentConfiguration.SourcePassword}";
+            var processInfo = new ProcessStartInfo("ExportFromAzureSQLDb.bat", args);
+            var process =  Process.Start(processInfo);
+            process.WaitForExit();
+            Console.WriteLine($"{currentDatabase} database done");
+        }
+
+    }
+}
